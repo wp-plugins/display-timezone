936384e02a2ded3ef26b4276f7849a575cee8252
midhund <midhund@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-08 20:27:21 +0000

    Added the first version files to the trunk.
    git-svn-id: https://plugins.svn.wordpress.org/display-timezone/trunk@286942 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ displayTimeZone.php
@@ -0,0 +1,100 @@
+<?php
+/*
+Plugin Name: Display Time(zone)
+Plugin URI: http://tutorboy.com
+Description: Simple plug-in to display current time with timezone in the upper right of your admin screen on every page. It takes the values from the option Timezone, Date Format, Time Format and starts the clock.
+Author: Midhun Devasia
+Version: 1.0.0
+Author URI: http://tutorboy.com
+License: GPLv2
+
+	Copyright 2010  Midhun Devasia  (email : midhun@tutorboy.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+    Contributer: Jacob Wright <http://jacwright.com/> whos wrote the javascript function for the dateformat.
+*/
+
+
+// This just echoes the current time according to the timezone set from the WordPress date settings.
+function tboy_getTimezone() {
+	date_default_timezone_set(get_option('timezone_string'));
+	$date = strtotime(date(get_option('date_format') . " " . get_option('time_format'))); 
+	
+	$jscript = "
+	<script>
+	var sec = 0, cd = new Date();
+	tboy_setTimezone();
+	tboy_getTimezone();
+	setInterval(\"tboy_getTimezone()\", 1000 );
+	
+	function tboy_setTimezone(){
+		cd.setYear(".date('Y', $date).");
+		cd.setMonth((".date('m', $date)."-1));
+		cd.setDate(".date('d', $date).");
+		cd.setHours(".date('h', $date).");
+		cd.setMinutes(".date('i', $date).");
+		sec = ".date('s', $date).";
+		cd.setSeconds(sec);
+	}
+	
+	function tboy_getTimezone(){
+		(sec == 60 )? sec = 1 : sec++;
+		cd.setSeconds(sec);
+		document.getElementById(\"tboy_displayTimezoneSpan\").innerHTML  = cd.format('". get_option('date_format') . " " .get_option('time_format')."'); 
+	}
+	
+	</script>";
+	
+	echo "<p id='tboy_displayTimezone'>
+		<b><span id=\"tboy_displayTimezoneSpan\">" . 
+		date(get_option('date_format') . " " . 
+		get_option('time_format')) . "</span></b> 
+		(" . date_default_timezone_get() .")</p>";
+		
+	echo $jscript ;
+}
+
+// We need some CSS to position the paragraph
+function tboy_displayTimezone_css() {
+	// This makes sure that the posinioning is also good for right-to-left languages
+	$x = ( is_rtl() ) ? 'left' : 'right';
+	echo "
+	<style type='text/css'>
+	#tboy_displayTimezone {
+		position: absolute;
+		top: 4.5em;
+		margin: 0;
+		padding: 0;
+		$x: 215px;
+		font-size: 11px;
+	}
+	</style>
+	";
+}
+
+function tboy_getTimezone_script_css(){
+	define(WPDTZ_URL, WP_PLUGIN_URL.'/display-timezone/');
+	wp_register_script('tboy_getTimezone_script', WPDTZ_URL. 'js/Date.format.js');
+	// enqueue the script
+	wp_enqueue_script('tboy_getTimezone_script');
+ 
+}
+
+add_action('init', 'tboy_getTimezone_script_css');
+add_action('admin_head', 'tboy_displayTimezone_css');
+// Now we set that function up to execute when the admin_footer action is called
+add_action('admin_footer', 'tboy_getTimezone');
+?>
---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== Display Time(zone) ===
+Contributors: midhund
+Tags: admin, timezone
+Tested up to: 2.8.4
+Stable tag: trunk
+
+Simple plug-in to display current time with timezone in the upper right of your admin screen on every page. 
+
+== Description ==
+
+Simple plug-in to display current time with timezone in the upper right of your admin screen on every page. It takes the values from the option Timezone, Date Format, Time Format and starts the clock. There is no configuration page.
+
+
+== Installation ==
+
+Installation is simple and should not take you more than 1 minutes.
+
+1. Upload whole `display-timezone` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Screenshots ==
+
+1. Screenshot Installation 
+2. Screenshot General Settings View
+2. Screenshot Display Time(zone) View on Admin page
